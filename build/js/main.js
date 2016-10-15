c9ddea7f32fc0c06e13e44b72c3d6caeb39d2502
Maksim Kachurin <kachurun@gmail.com>
2016-10-15 14:46:41 +0200

    main js tips

--- build/js/main.js
+++ build/js/main.js
@@ -2,24 +2,23 @@
 
 (function () {
     $(function () {
-        var paginator = $('#container').pjax({
+        var lazyLoadEnabled = false; // ключ, что ленивая подгрузка не активна
+        var paginator = $('#container').pjax({ // появился обьект paginator, можно к нему обращаться
             pagination: '.pagination-container .pagination a',
             lazyLoad: '.load-more',
             lazyContainer: '.pagination-container',
-            lazyDynamic: false,
-            lazyDynamicTimeout: 500,
-            special_params: {
-                isAjax: true
-            },
-            cache: {
-                enabled: false
+            lazyDynamic: false, // по умолчанию отключено
+            lazyDynamicTimeout: 500, // настраиваем это заранее (можно и потом, но смысл)
+            specialParams: { // special_params -> specialParams
+                is_ajax: true
             },
             callbacks: {
                 afterLoad: function afterLoad() {
-                    paginator.setParams({ 'lazyDynamic': true });
-                },
-                onError: function onError() {
-                    // console.log(err);
+                    // если ленивая подгрузка еще не включена - включаем
+                    if (!lazyLoadEnabled) {
+                        paginator.setParams({ 'lazyDynamic': true });
+                        lazyLoadEnabled = true;
+                    }
                 }
             }
         });
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,22 @@
 (function() {
     $(function() {
-        let paginator = $('#container').pjax({
+        let lazyLoadEnabled = false; // ключ, что ленивая подгрузка не активна
+        let paginator = $('#container').pjax({ // появился обьект paginator, можно к нему обращаться
             pagination: '.pagination-container .pagination a',
             lazyLoad: '.load-more',
             lazyContainer: '.pagination-container',
-            lazyDynamic: false,
-            lazyDynamicTimeout: 500,
-            special_params: {
-                isAjax: true
-            },
-            cache: {
-                enabled: false
+            lazyDynamic: false, // по умолчанию отключено
+            lazyDynamicTimeout: 500, // настраиваем это заранее (можно и потом, но смысл)
+            specialParams: { // special_params -> specialParams
+                is_ajax: true
             },
             callbacks: {
-                afterLoad: function() {
-                    paginator.setParams({ 'lazyDynamic': true });
-                },
-                onError: function() {
-                    // console.log(err);
+                afterLoad: () => {
+                    // если ленивая подгрузка еще не включена - включаем
+                    if (!lazyLoadEnabled) {
+                        paginator.setParams({ 'lazyDynamic': true });
+                        lazyLoadEnabled = true;
+                    }
                 }
             }
         });
